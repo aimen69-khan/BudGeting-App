f3135b14062951f460bccf18cd3c324d604590f4
Ahmed Raza <aimen69khan@gmail.com>
2026-05-29 11:40:20 +0500

    all-done delete-&-edit Not_completed

--- app.js
+++ app.js
@@ -2,22 +2,54 @@ function budget(){
     let firstInput = document.querySelector('.firstInput');
     let inputValue = firstInput.value;
     let budgetValue = document.getElementById('budgetValue');
-    budgetValue.textContent = inputValue;
+    if(inputValue === ''){
+        alert('" NO VALUE FOUND "');
+    }else{
+        budgetValue.textContent = inputValue;
+    }
 }
 
 function expenses(){
     let secondInput = document.getElementById('secondInput');
     let secondInputValue = secondInput.value;
-    let expensesValue = document.getElementById('expensesValue');
+    let expensesValue = document.querySelector('.expensesValue');
     expensesValue.textContent = secondInputValue;
 }
 
 function balance(){
     budget();
     expenses();
-
     let budgetNumber = parseFloat(document.getElementById('budgetValue').textContent);
-    let expensesNumber = parseFloat(document.getElementById('expensesValue').textContent);
+    let expensesNumber = parseFloat(document.querySelector('.expensesValue').textContent);
     let balance = document.getElementById('balanceValue');
     balance.textContent = budgetNumber - expensesNumber;
 }
+
+function titleProduct(){
+    let title = document.getElementById('inputTitle');
+    let inputTitle = title.value.trim();
+    let secondInput = document.getElementById('secondInput');
+    let secondInputValue = secondInput.value;
+    let titleOfProduct = document.getElementById('productName');
+    if(inputTitle === '' || secondInputValue === ''){
+        alert("Enter A Value!");
+        return;
+    }else if(!isNaN(inputTitle)){
+        alert("Value is a Number!");
+        return;
+    }else{
+      titleOfProduct.textContent = inputTitle;
+      secondExp.textContent = secondInputValue;
+      expenseItem.classList.remove('hidden');
+}
+}
+
+
+function secondExpeses(){
+    expenses();
+    let secondExpesesCost = document.querySelector('.expValue');
+    let secondExpesesCostValue = secondExpesesCost.value;
+    let secondExpesesShows = document.getElementById('secondExp');
+    secondExpesesShows.textContent = secondExpesesCostValue;
+}
+
